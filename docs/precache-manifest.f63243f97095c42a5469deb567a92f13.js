aaef357d5278f561680747d19a0aa21833276037
Mario <mamaldonf@gmail.com>
2020-10-10 21:35:26 -0500

    docs agregado

---
+++ docs/precache-manifest.f63243f97095c42a5469deb567a92f13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5299e1e1b6e01eb454dcbef62dc8b03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4877c6819907add06965",
+    "url": "/static/css/main.6999dc75.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4877c6819907add06965",
+    "url": "/static/js/main.9da2f5c1.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
